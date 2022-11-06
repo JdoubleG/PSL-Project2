7b704991c4767978a1317af1671d484d94adc76e
J. Garijo-Garde <jgarijogarde@outlook.com>
2022-11-06 16:53:55 -0500

    SVD preprocessing

--- mymain.R
+++ mymain.R
@@ -204,6 +204,95 @@ mypredict5 = function() {
 
 
 ### Using SVD/PCA #782
+svd.preproc = function(depts, d=8){
+  # d indicates the top d components to select
+  
+  dept_data <- train %>%
+    select(Store, Dept, Date, Weekly_Sales) %>%
+    spread(Date, Weekly_Sales) %>%
+    #replace(is.infinite(.), 0) %>%
+    as_tibble()
+  dept_data[is.na(dept_data)] <- 0
+  #dept_data[is.infinite(dept_data)] <- 0
+  
+  new_train = NULL
+  for (dept in depts){
+    # matrix X
+    X <- dept_data %>%
+      filter(Dept == dept)
+    
+    if (nrow(X) > d) {
+      temp = X[, 3:ncol(dept_data)]
+      store.mean = rowMeans(temp)
+      #store.mean[is.na(store.means)] <- 0
+      temp = temp - store.mean
+      
+      svd = svd(temp)
+      U = svd$u
+      V = svd$v
+      D = matrix(diag(svd$d[1:d]), ncol=d)
+      train_svd = U[,1:d] %*% D[1:d, 1:d] %*% t(V[,1:d]) + store.mean
+      
+      X[, -c(1, 2)] = train_svd
+    }
+    new_train = rbind(new_train, X)
+  }
+  new_train = new_train %>% as_tibble() %>% gather(Date, Weekly_Sales, -Store, -Dept)
+  return(new_train)
+}
+
 mypredict = function() {
-  ;
+  start_date <- ymd("2011-03-01") %m+% months(2 * (t - 1))
+  end_date <- ymd("2011-05-01") %m+% months(2 * (t - 1))
+  test_current <- test %>%
+    filter(Date >= start_date & Date < end_date) %>%
+    select(-IsHoliday)
+  
+  # use preproc
+  new_train = svd.preproc(unique(test_current$Dept))
+  
+  # find the unique pairs of (Store, Dept) combo that appeared in both training and test sets
+  train_pairs <- new_train[, 1:2] %>% count(Store, Dept) %>% filter(n != 0)
+  test_pairs <- test_current[, 1:2] %>% count(Store, Dept) %>% filter(n != 0)
+  unique_pairs <- intersect(train_pairs[, 1:2], test_pairs[, 1:2])
+  
+  # pick out the needed training samples, convert to dummy coding, then put them into a list
+  train_split <- unique_pairs %>% 
+    left_join(new_train, by = c('Store', 'Dept')) %>% 
+    mutate(Wk = factor(ifelse(year(Date) == 2010, week(Date) - 1, week(Date)), levels = 1:52)) %>% 
+    mutate(Yr = year(Date))
+  train_split = as_tibble(model.matrix(~ Weekly_Sales + Store + Dept + Yr + Wk, train_split)) %>% group_split(Store, Dept)
+  
+  # do the same for the test set
+  test_split <- unique_pairs %>% 
+    left_join(test_current, by = c('Store', 'Dept')) %>% 
+    mutate(Wk = factor(ifelse(year(Date) == 2010, week(Date) - 1, week(Date)), levels = 1:52)) %>% 
+    mutate(Yr = year(Date))
+  test_split = as_tibble(model.matrix(~ Store + Dept + Yr + Wk, test_split)) %>% mutate(Date = test_split$Date) %>% group_split(Store, Dept)
+  
+  # pre-allocate a list to store the predictions
+  test_pred <- vector(mode = "list", length = nrow(unique_pairs))
+  
+  # perform regression for each split, note we used lm.fit instead of lm
+  for (i in 1:nrow(unique_pairs)) {
+    tmp_train <- train_split[[i]]
+    tmp_test <- test_split[[i]]
+    
+    # shift for fold 5
+    if (t==5){
+      shift = 1/7
+      tmp_test[, "Wk51"] = tmp_test[, "Wk51"] * (1-shift) + 
+        tmp_test[, "Wk52"] * shift
+    }    
+    
+    mycoef <- lm.fit(as.matrix(tmp_train[, -(2:4)]), tmp_train$Weekly_Sales)$coefficients
+    mycoef[is.na(mycoef)] <- 0
+    tmp_pred <- mycoef[1] + as.matrix(tmp_test[, 4:55]) %*% mycoef[-1]
+    
+    test_pred[[i]] <- cbind(tmp_test[, 2:3], Date = tmp_test$Date, Weekly_Pred = tmp_pred[,1])
+  }
+  
+  # turn the list into a table at once, 
+  # this is much more efficient then keep concatenating small tables
+  test_pred <- bind_rows(test_pred)
 }
